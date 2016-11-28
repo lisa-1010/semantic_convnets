62fb5c91e770e7c3621a8c2967f46750a9a24b38
Ajay Sohmshetty <ajay14@stanford.edu>
2016-11-28 11:32:37 -0800

    Updating setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,3 @@ sudo pip install --upgrade pip
 sudo pip install h5py
 sudo pip install sklearn
 sudo pip install git+https://github.com/tflearn/tflearn.git
-git clone https://github.com/lisa-1010/semantic_convnets
