041eb394b8a595873a65f5a45eec756710fe79c2
Ajay Sohmshetty <ajay14@stanford.edu>
2016-12-04 22:18:47 -0800

    Adding setup.sh for aws and .gitignore updates

--- setup.sh
+++ setup.sh
@@ -1,5 +1,18 @@
-cd ~/
-sudo pip install --upgrade pip 
-sudo pip install h5py
-sudo pip install sklearn
-sudo pip install git+https://github.com/tflearn/tflearn.git
+#!/bin/bash
+IPADDR="35.165.30.83"
+
+ssh -i ~/.ssh/MyndbookEBKeyPair.pem ubuntu@$IPADDR <<-'ENDSSH'
+    #commands to run on remote host
+    cd ~/
+    sudo pip install --upgrade pip
+    sudo pip install h5py
+    sudo pip install sklearn
+    sudo pip install git+https://github.com/tflearn/tflearn.git
+    git clone https://github.com/lisa-1010/semantic_convnets
+ENDSSH
+
+scp -r -i ~/.ssh/MyndbookEBKeyPair.pem data ubuntu@$IPADDR:~/semantic_convnets
+
+
+
+
